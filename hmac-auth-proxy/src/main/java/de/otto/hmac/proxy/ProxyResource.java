eae7c03adfdd23bf8bf30647940b7549f56a4265
Tom Vollerthun <tom.vollerthun@otto.de>
2013-01-28 10:56:23 +0100

    685|dr/tv|added test for checking that we use complete path

--- hmac-auth-proxy/src/main/java/de/otto/hmac/proxy/ProxyResource.java
+++ hmac-auth-proxy/src/main/java/de/otto/hmac/proxy/ProxyResource.java
@@ -24,55 +24,55 @@ public class ProxyResource {
     @Path("{resource:.*}")
     @GET
     public String getRequest(@Context UriInfo uriInfo, @Context Request request) {
-        return createBuilder(uriInfo, request).get(String.class);
+        return createBuilder(uriInfo, request.getMethod()).get(String.class);
     }
 
     @Path("{resource:.*}")
     @POST
     public String postRequest(String body, @Context UriInfo uriInfo, @Context Request request) {
-        return createBuilder(uriInfo, request, body).post(String.class);
+        return createBuilder(uriInfo, body, request.getMethod()).post(String.class);
     }
 
     @Path("{resource:.*}")
     @PUT
     public String putRequest(String body, @Context UriInfo uriInfo, @Context Request request) {
-        return createBuilder(uriInfo, request, body).put(String.class);
+        return createBuilder(uriInfo, body, request.getMethod()).put(String.class);
     }
 
 
     @Path("{resource:.*}")
     @DELETE
     public String deleteRequest(@Context UriInfo uriInfo, @Context Request request) {
-        return createBuilder(uriInfo, request).delete(String.class);
+        return createBuilder(uriInfo, request.getMethod()).delete(String.class);
     }
 
     @Path("{resource:.*}")
     @HEAD
     public ClientResponse headRequest(@Context UriInfo uriInfo, @Context Request request) {
-        return createBuilder(uriInfo, request).head();
+        return createBuilder(uriInfo, request.getMethod()).head();
     }
 
     @Path("{resource:.*}")
     @OPTIONS
     public String optionsRequest(@Context UriInfo uriInfo, @Context Request request) {
-        return createBuilder(uriInfo, request).options(String.class);
+        return createBuilder(uriInfo, request.getMethod()).options(String.class);
     }
 
 
-    private WebResource.Builder createBuilder(UriInfo uriInfo, Request request, String body) {
+    private WebResource.Builder createBuilder(UriInfo uriInfo, String body, String method) {
         URI targetUri = withTargetHostAndPort(uriInfo.getRequestUriBuilder());
-        return webResourceWithAuth(body, request.getMethod(), targetUri);
+        return webResourceWithAuth(body, method, targetUri);
     }
 
-    private WebResource.Builder createBuilder(UriInfo uriInfo, Request request) {
-        return createBuilder(uriInfo, request, "");
+    protected WebResource.Builder createBuilder(UriInfo uriInfo, String method) {
+        return createBuilder(uriInfo, "", method);
     }
 
 
     protected WebResource.Builder webResourceWithAuth(String body, String method, URI targetUri) {
         WebResource.Builder builder = client
                 .withMethod(method)
-                .withUri(targetUri.toString())
+                .withUri(targetUri.getPath())
                 .withBody(body)
                 .auth(ProxyConfiguration.getUser(), ProxyConfiguration.getPassword())
                 .authenticatedResource(targetUri.toString());
--- hmac-auth-proxy/src/test/java/de/otto/hmac/proxy/ProxyResourceTest.java
+++ hmac-auth-proxy/src/test/java/de/otto/hmac/proxy/ProxyResourceTest.java
@@ -1,15 +1,22 @@
 package de.otto.hmac.proxy;
 
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.client.impl.ClientRequestImpl;
 import org.testng.annotations.Test;
 
 import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
-import static org.mockito.Matchers.any;
+import static javax.ws.rs.core.UriBuilder.fromUri;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.*;
 
 @Test
 public class ProxyResourceTest {
-    
+
     @Test
     public void shouldAdjustTargetServerAndPort() {
         ProxyConfiguration.setPort(18);
@@ -21,6 +28,30 @@ public class ProxyResourceTest {
 
         verify(builder, times(1)).host("SOME_HOST");
         verify(builder, times(1)).port(18);
+    }
+
+    @Test
+    public void shouldUseCompletePathForRequest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        UriInfo uriInfo = mock(UriInfo.class);
+        when(uriInfo.getRequestUriBuilder()).thenReturn(fromUri("http://localhost:9998/p13n/menuItemConfigs/damenmode"));
+
+        ProxyConfiguration.setPort(80);
+        ProxyConfiguration.setUser("user");
+        ProxyConfiguration.setPassword("secret");
+        ProxyConfiguration.setTargetHost("develop.lhotse.ov.otto.de");
+
+        ProxyResource proxyResource = new ProxyResource();
+        WebResource.Builder target = proxyResource.createBuilder(uriInfo, "GET");
+
+        ClientRequestImpl clientRequest = getRequestFromWebresourceBuilder(target);
+
+        assertThat(clientRequest.getURI().toString(), is("http://develop.lhotse.ov.otto.de:80/p13n/menuItemConfigs/damenmode"));
+    }
+
+    private ClientRequestImpl getRequestFromWebresourceBuilder(WebResource.Builder target) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Method buildMethod = WebResource.Builder.class.getDeclaredMethod("build", String.class);
+        buildMethod.setAccessible(true);
+        return (ClientRequestImpl) buildMethod.invoke(target, "GET");
     }
 
 
