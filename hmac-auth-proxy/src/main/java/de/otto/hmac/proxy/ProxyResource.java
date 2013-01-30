26649ed9126c990a792c41477c94d03739ad04c5
Tom Vollerthun <tom.vollerthun@otto.de>
2013-01-30 12:34:49 +0100

    685|dr/tv|added missing file

--- hmac-auth-proxy/src/main/java/de/otto/hmac/proxy/ProxyResource.java
+++ hmac-auth-proxy/src/main/java/de/otto/hmac/proxy/ProxyResource.java
@@ -8,6 +8,7 @@ import de.otto.hmac.authentication.HMACJerseyClient;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -42,7 +43,6 @@ public class ProxyResource {
     @Path("{resource:.*}")
     @PUT
     public Response putRequest(String body, @Context UriInfo uriInfo, @Context Request request, @Context HttpHeaders headers) {
-
         ClientResponse clientResponse = createBuilder(uriInfo, body, request.getMethod(), headers).put(ClientResponse.class, body);
         return clientResponseToResponse(clientResponse);
     }
@@ -50,7 +50,7 @@ public class ProxyResource {
     @Path("{resource:.*}")
     @DELETE
     public Response deleteRequest(@Context UriInfo uriInfo, @Context Request request, @Context HttpHeaders headers) {
-        ClientResponse clientResponse = createBuilder(uriInfo, request.getMethod(), headers).delete(ClientResponse.class);
+        ClientResponse clientResponse = createBuilder(uriInfo, request.getMethod(), headers, HttpHeaders.CONTENT_LENGTH).delete(ClientResponse.class);
         return clientResponseToResponse(clientResponse);
     }
 
@@ -72,7 +72,7 @@ public class ProxyResource {
     private static Response clientResponseToResponse(ClientResponse clientResponse) {
         Response.ResponseBuilder rb = Response.status(clientResponse.getStatus());
         copyResponseHeaders(clientResponse, rb);
-        String content = clientResponse.getStatus() != 204 ? clientResponse.getEntity(String.class) : "";
+        String content = clientResponse.getStatus() != 204 ? clientResponse.getEntity(String.class) : null;
         System.out.println(String.format("Retrieved answer: HTTP-Code [%d]\nContent: \n%s\n\n", clientResponse.getStatus(), content));
         rb.entity(content);
         return rb.build();
@@ -86,13 +86,13 @@ public class ProxyResource {
         }
     }
 
-    private static void copyRequestHeaders(HttpHeaders headers, WebResource.Builder builder) {
+    private static void copyRequestHeaders(HttpHeaders headers, WebResource.Builder builder, List<String> ignoreHeaders) {
         if (headers == null) {
             return;
         }
 
         for (Map.Entry<String, List<String>> entry : headers.getRequestHeaders().entrySet()) {
-            if (ACCEPT_ENCODING.equalsIgnoreCase(entry.getKey())) {
+            if (ignoreHeaders.contains(entry.getKey().toLowerCase())) {
                 continue;
             }
             for (String value : entry.getValue()) {
@@ -102,18 +102,29 @@ public class ProxyResource {
     }
 
 
-    private WebResource.Builder createBuilder(UriInfo uriInfo, String body, String method, HttpHeaders headers) {
+    private WebResource.Builder createBuilder(UriInfo uriInfo, String body, String method, HttpHeaders headers, String... ignoreHeaders) {
         URI targetUri = withTargetHostAndPort(uriInfo.getRequestUriBuilder());
         System.out.println("Sending request to " + targetUri);
         WebResource.Builder builder = webResourceWithAuth(body, method, targetUri);
 
-        copyRequestHeaders(headers, builder);
+        ArrayList<String> allIgnoreHeaders = of(ignoreHeaders);
+        allIgnoreHeaders.add(ACCEPT_ENCODING.toLowerCase());
+
+        copyRequestHeaders(headers, builder, allIgnoreHeaders);
 
         return builder;
     }
 
-    protected WebResource.Builder createBuilder(UriInfo uriInfo, String method, HttpHeaders headers) {
-        return createBuilder(uriInfo, "", method, headers);
+    private static ArrayList<String> of(String[] ignoreHeaders) {
+        ArrayList<String> allIgnoreHeaders = new ArrayList<>();
+        for (String ignoreHeader : ignoreHeaders) {
+            allIgnoreHeaders.add(ignoreHeader.toLowerCase());
+        }
+        return allIgnoreHeaders;
+    }
+
+    protected WebResource.Builder createBuilder(UriInfo uriInfo, String method, HttpHeaders headers, String... ignoreHeaders) {
+        return createBuilder(uriInfo, "", method, headers, ignoreHeaders);
     }
 
 
