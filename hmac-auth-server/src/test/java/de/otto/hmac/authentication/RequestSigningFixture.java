f79dfbce435ef0222dce309d0c4cebba1b514db6
Micha Regel <micha.regel@ottogroup.com>
2014-02-21 10:55:50 +0100

    read a byte array instead of String from requests inputstream

--- hmac-auth-server/src/test/java/de/otto/hmac/authentication/RequestSigningFixture.java
+++ hmac-auth-server/src/test/java/de/otto/hmac/authentication/RequestSigningFixture.java
@@ -11,10 +11,10 @@ public class RequestSigningFixture {
         return createSignature(request.getRequestURI(), request.getMethod(), user, key, request.getHeader("x-hmac-auth-date"), request.getBody());
     }
 
-    public static String createSignature(String uri, String method, String user, String key, String date, String content) throws IOException {
+    public static String createSignature(String uri, String method, String user, String key, String date, byte[] content) throws IOException {
         MockHttpServletRequest request = new MockHttpServletRequest(method, uri);
         request.addHeader("x-hmac-auth-date", date);
-        request.setContent(content.getBytes());
+        request.setContent(content);
         return user + ":" + RequestSigningUtil.createRequestSignature(WrappedRequest.wrap(request), key);
     }
 
